55dcd7790002f944c87b1731944985feca71f5fd
jihyunglee <jhlee7125@gmail.com>
2022-11-26 01:44:37 +0900

    feat: styled-component ImgProduct

--- src/components/Card/ProductImage/ProductImage.jsx
+++ src/components/Card/ProductImage/ProductImage.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function ProductImage({ thumbnailImg, productName }) {
-  // console.log(thumbnailImg)
+  const ImgProduct = styled.img`
+    position: relative;
+    width: 380px;
+    height: 380px;
+    border: 1px solid var(--border-color);
+    border-radius: 10px;
+    overflow: hidden;
+  `;
+
   const url = 'https://test.api.weniv.co.kr/' + thumbnailImg;
-  return <img src={url} alt={productName} />;
+  return <ImgProduct src={url} alt={productName} />;
 }
