61008e837ede95e1130344d5a2e00d2ba48fa9fb
jihyunglee <jhlee7125@gmail.com>
2022-11-26 11:34:36 +0900

    fix: StyledProductImg로 스타일컴포넌트 이름 변경 및 ProductImage 컴포넌트 함수 밖으로 스타일컴포넌트 배치

--- src/components/Card/ProductImage/ProductImage.jsx
+++ src/components/Card/ProductImage/ProductImage.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function ProductImage({ thumbnailImg, productName }) {
-  const ImgProduct = styled.img`
-    position: relative;
-    width: 380px;
-    height: 380px;
-    border: 1px solid var(--border-color);
-    border-radius: 10px;
-    overflow: hidden;
-  `;
+const StyledProductImg = styled.img`
+  position: relative;
+  width: 380px;
+  height: 380px;
+  border: 1px solid var(--border-color);
+  border-radius: 10px;
+  overflow: hidden;
+`;
 
+export default function ProductImage({ thumbnailImg, productName }) {
   const url = 'https://test.api.weniv.co.kr/' + thumbnailImg;
-  return <ImgProduct src={url} alt={productName} />;
+  return <StyledProductImg src={url} alt={productName} />;
 }
