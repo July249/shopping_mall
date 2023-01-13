db688e2c558fc651f156ab0a5c82a8c1f75625c5
jihyunglee <jhlee7125@gmail.com>
2023-01-13 19:52:55 +0900

    design: TopBar 구현

---
+++ src/components/Organisms/TopBar/TopBarStyle.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { NavEl } from "../../Atoms/_Nav/NavEl";
+import { SectionEl } from "../../Atoms/_Section/SectionEl";
+import { UlEl } from "../../Atoms/_Ul/UlEl";
+import { LiEl } from "../../Atoms/_Li/LiEl";
+
+export const TopBarContainer = styled(NavEl)`
+  display: flex;
+  flex-direction: row;
+  flex-shrink: 0;
+  justify-content: space-between;
+  padding: 2rem 0;
+  margin: 0 16.66%;
+`;
+
+export const TopBarSection = styled(SectionEl)`
+  display: flex;
+  flex-direction: row;
+  flex-shrink: 0;
+  align-items: center;
+  > img {
+    margin-right: 3rem;
+  }
+`;
+
+export const TopBarMenuUl = styled(UlEl)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TopBarMenuLi = styled(LiEl)`
+  flex-shrink: 0;
+  margin-right: 2.6rem;
+  :last-child {
+    margin-right: 0;
+  }
+`;
