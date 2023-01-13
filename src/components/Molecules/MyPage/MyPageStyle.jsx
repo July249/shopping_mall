5a8dc174e75e873a4e39a9a6ffed5823be615c82
jihyunglee <jhlee7125@gmail.com>
2023-01-13 19:27:27 +0900

    design: MyPage 버튼 스타일 변경

--- src/components/Molecules/MyPage/MyPageStyle.jsx
+++ src/components/Molecules/MyPage/MyPageStyle.jsx
@@ -2,9 +2,13 @@ import styled from "styled-components";
 import { ButtonEl } from "../../Atoms/_Button/ButtonEl";
 import { ImgEl } from "../../Atoms/_Img/ImgEl";
 
-export const MyPageBtn = styled(ButtonEl)``;
+export const MyPageBtn = styled(ButtonEl)`
+  background: none;
+`;
 
 export const MyPageImg = styled(ImgEl)`
-  width: 100%;
-  height: 100%;
+  display: block;
+  width: 3.2rem;
+  height: 3.2rem;
+  margin: 0 auto 0.4rem;
 `;
