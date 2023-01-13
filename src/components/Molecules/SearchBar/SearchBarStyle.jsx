636b8472d10064b6686fccc5a2dc255ed0d91043
jihyunglee <jhlee7125@gmail.com>
2023-01-13 13:00:03 +0900

    design: SearchBar 생성 및 스타일 적용

---
+++ src/components/Molecules/SearchBar/SearchBarStyle.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { InputEl } from "../../Atoms/_Input/InputEl";
+import { ButtonEl } from "../../Atoms/_Button/ButtonEl";
+import { ImgEl } from "../../Atoms/_Img/ImgEl";
+import { DivEl } from "../../Atoms/_Div/DivEl";
+
+export const SearchBox = styled(DivEl)`
+  position: relative;
+  width: 40rem;
+`;
+
+export const SearchInput = styled(InputEl)`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 13px 22px;
+  border: 2px solid var(--main-color);
+  border-radius: 50px;
+  font-size: 1.6rem;
+  line-height: 2rem;
+  outline: var(--main-color);
+  color: var(--sub-txt-color);
+`;
+
+export const SearchBtn = styled(ButtonEl)`
+  width: 2.8rem;
+  height: 2.8rem;
+  position: absolute;
+  top: 0.9rem;
+  right: 2.2rem;
+  background: none;
+`;
+
+export const SearchImg = styled(ImgEl)`
+  width: 100%;
+  height: 100%;
+`;
