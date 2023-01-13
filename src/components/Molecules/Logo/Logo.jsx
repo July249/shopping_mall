af1a8b857ae2aa4cdc698f4329cffcd1df032532
jihyunglee <jhlee7125@gmail.com>
2023-01-13 12:58:01 +0900

    design: Logo 생성 및 스타일 적용

---
+++ src/components/Molecules/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import { LogoImg } from "./LogoStyle";
+import Hodu from "../../../assets/icons/Logo-hodu.png";
+
+const Logo = () => {
+  return <LogoImg src={Hodu} alt="호두 메인 로고" />;
+};
+
+export default Logo;
---
+++ src/components/Molecules/Logo/LogoStyle.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { ImgEl } from "../../Atoms/_Img/ImgEl";
+
+export const LogoImg = styled(ImgEl)`
+  width: 12.4rem;
+  height: 3.8rem;
+`;
