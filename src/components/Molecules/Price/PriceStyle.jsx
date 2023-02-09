d2d32516ae56aacb62c83c8c457ceab4d66c8833
jihyunglee <jhlee7125@gmail.com>
2023-02-09 20:00:08 +0900

    design: Price 컴포넌트 isEmphasized 속성 추가에 따른 글자색 변경 스타일 추가

--- src/components/Molecules/Price/PriceStyle.jsx
+++ src/components/Molecules/Price/PriceStyle.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { SpanEl } from "../../Atoms/_Span/SpanEl";
 
 export const PriceText = styled(SpanEl)`
+  display: block;
   font-weight: 700;
   font-size: ${(props) => {
     if (props.size === "lr") {
@@ -10,6 +11,8 @@ export const PriceText = styled(SpanEl)`
       return "2.4rem";
     } else if (props.size === "sm") {
       return "1.8rem";
+    } else if (props.size === "xs") {
+      return "1.6rem";
     }
   }};
   line-height: ${(props) => {
@@ -19,12 +22,41 @@ export const PriceText = styled(SpanEl)`
       return "3.0rem";
     } else if (props.size === "sm") {
       return "2.3rem";
+    } else if (props.size === "xs") {
+      return "2rem";
+    }
+  }};
+  color: ${(props) => {
+    if (props.isEmphasized) {
+      return "var(--emphasized-txt-color)";
+    } else {
+      return "var(--txt-color)";
     }
   }};
   ::after {
     content: "원";
     font-weight: 400;
-    font-size: 1.8rem;
-    line-height: 2.3rem;
+    font-size: ${(props) => {
+      if (props.size === "lr") {
+        return "1.8rem";
+      } else if (props.size === "md") {
+        return "1.6rem";
+      } else if (props.size === "sm") {
+        return "1.4rem";
+      } else if (props.size === "xs") {
+        return "1.6rem";
+      }
+    }};
+    line-height: ${(props) => {
+      if (props.size === "lr") {
+        return "2.3rem";
+      } else if (props.size === "md") {
+        return "2rem";
+      } else if (props.size === "sm") {
+        return "1.8rem";
+      } else if (props.size === "xs") {
+        return "2rem";
+      }
+    }};
   }
 `;
